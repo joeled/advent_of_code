b8c8a635b9f079cd3702415dab02a10727350790
Joe <joseph@orderlyhealth.com>
2022-12-07 18:02:30 -0700

    day7 wip

--- day_7/puzzle_1.py
+++ day_7/puzzle_1.py
@@ -1,29 +1,28 @@
 curr_full_path = ""
-curr_dir = ""
 dir_top_level_sizes = {}
 dir_parents = {}
 answer_dir_sum = 0
+dir_names = ["/"]
 
 
 def process_command(line):
-    global curr_full_path, curr_dir
+    global curr_full_path, dir_names
 
     command_split = line.split(" ")
 
     if command_split[1] == 'cd':
         if command_split[2] == '..':
             full_path_split = curr_full_path.split("/")
-            curr_full_path = "/".join(full_path_split[0:len(full_path_split)-2])
-            curr_dir = full_path_split[len(full_path_split)-2]
+            curr_full_path = "/".join(full_path_split[0:len(full_path_split)-2]) + "/"
         elif command_split[2] == '/':
-            curr_dir = "/"
             curr_full_path = "/"
         else:
             curr_dir = command_split[2]
-            curr_full_path += "/"+curr_dir
+            curr_full_path += curr_dir + "/"
+            dir_names.append("/"+curr_dir+"/")
     else:
-        # Initialize dir size
-        dir_top_level_sizes.update({curr_dir: 0})
+        # ls - Initialize dir size
+        dir_top_level_sizes.update({curr_full_path: 0})
 
 
 def process_file_or_dir(line):
@@ -31,12 +30,10 @@ def process_file_or_dir(line):
 
     dir_top_level_sum = 0
 
-    if line_split[0] == 'dir':
-        dir_parents.update({line_split[1]: curr_dir})
-    else:
+    if line_split[0] != 'dir':
         dir_top_level_sum += int(line_split[0])
 
-    dir_top_level_sizes.update({curr_dir: dir_top_level_sum})
+    dir_top_level_sizes.update({curr_full_path: dir_top_level_sum})
 
 
 def process_line(line):
@@ -55,11 +52,19 @@ def process_input():
         process_line(line)
 
 
-def recurse(dir):
-    pass
+def get_answer():
+    global answer_dir_sum
+
+    for dir_name in dir_names:
+        dir_sum = 0
+        for dir_size in dir_top_level_sizes:
+            if dir_name in dir_size:
+                dir_sum += dir_top_level_sizes.get(dir_size)
+        if dir_sum <= 100000:
+            answer_dir_sum += dir_sum
 
 
 if __name__ == "__main__":
     process_input()
-    recurse("/")
+    get_answer()
     print(answer_dir_sum)
