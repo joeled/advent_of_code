d53cf9617c8a9e1fb9abb77060266c13509192ff
Joe <joseph@orderlyhealth.com>
2022-12-12 11:08:48 -0700

    day 12

---
+++ day_12/puzzle_1.py
@@ -0,0 +1,70 @@
+grid = []
+start = []
+end = []
+visited = set()
+
+
+def can_move(element, new_element):
+    return grid[new_element[0]][new_element[1]] - grid[element[0]][element[1]] <= 1
+
+
+def in_bounds(element):
+    return len(grid) > element[0] >= 0 and len(grid[0]) > element[1] >= 0
+
+
+def add_neighbors_to_queue(element, queue):
+    dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    for dir in dirs:
+        new_element = [element[0] + dir[0], element[1] + dir[1]]
+        if in_bounds(new_element) and can_move(element, new_element) \
+                and str(new_element[0]) + " " + str(new_element[1]) not in visited \
+                and new_element not in queue:
+            queue.append(new_element)
+
+    return queue
+
+
+def bfs():
+    queue = [start]
+
+    iteration = 0
+    while len(queue) > 0:
+        queue_size = len(queue)
+        # print("Iteration: " + str(iteration) + " Queue size: " + str(queue_size))
+        for idx in range(0, queue_size):
+            element = queue.pop(0)
+            visited.add(str(element[0]) + " " + str(element[1]))
+            if element[0] == end[0] and element[1] == end[1]:
+                print(iteration)
+                return
+            else:
+                add_neighbors_to_queue(element, queue)
+        iteration += 1
+
+
+def parse_square(row, col, square):
+    global start, end
+    if square == 'S':
+        start = [row, col]
+        return ord('a')
+    elif square == 'E':
+        end = [row, col]
+        return ord('z')
+    else:
+        return ord(square)
+
+
+def process_input():
+    with open('inputs/puzzle_1.txt') as f:
+        lines = f.readlines()
+
+    for row in range(0, len(lines)):
+        line = lines[row]
+        line = line.replace("\n", "")
+        grid.append([parse_square(row, col, square) for col, square in enumerate(line)])
+
+    bfs()
+
+
+if __name__ == "__main__":
+    process_input()
