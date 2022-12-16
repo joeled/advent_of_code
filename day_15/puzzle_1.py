7721b0f0d687645924af3dd27d823dee9c256c73
Joe <joseph@orderlyhealth.com>
2022-12-16 13:51:33 -0700

    wip

--- day_15/puzzle_1.py
+++ day_15/puzzle_1.py
@@ -1,51 +1,20 @@
-visited = set()
+from interval import interval
 
 
-def add_neighbors_to_queue(element, queue):
-    dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    for dir in dirs:
-        new_element = [element[0] + dir[0], element[1] + dir[1]]
-        if str(new_element[0]) + " " + str(new_element[1]) not in visited \
-                and new_element not in queue:
-            queue.append(new_element)
-
-    return queue
-
-
-def mark_impossible_locations(grid, pair, grid_radius):
-    global visited
-
-    queue = [pair[0]]
-    visited = set()
-    beacon = pair[1]
-    found_beacon = False
-
-    while len(queue) > 0:
-
-        if found_beacon:
-            break
-
-        queue_size = len(queue)
-        for idx in range(0, queue_size):
-            element = queue.pop(0)
-            visited.add(str(element[0]) + " " + str(element[1]))
-            add_neighbors_to_queue(element, queue)
-
-            if element[0] == beacon[0] and element[1] == beacon[1]:
-                found_beacon = True
-            elif grid[element[0] + grid_radius-1][element[1] + grid_radius+1] == '.':
-                grid[element[0] + grid_radius-1][element[1] + grid_radius+1] = '#'
+def manhattan_distance(vertex_one, vertex_two):
+    return abs(vertex_one[0] - vertex_two[0]) + abs(vertex_one[1] - vertex_two[1])
 
 
 def process_input():
     with open('inputs/puzzle_1.txt') as f:
         lines = f.readlines()
 
-    grid_radius = 100000
+    answer_row = 2000000
 
-    grid = [['.' for col in range(-grid_radius, grid_radius)] for row in range(-grid_radius, grid_radius)]
+    rows = {}
 
-    sensor_beacon_pairs = []
+    sensors = []
+    beacons = []
 
     for line in lines:
         line = line.replace('\n', '')
@@ -54,17 +23,31 @@ def process_input():
         sensor = [int(split_line[0].split("=")[2]), int(split_line[0].split("=")[1].split(",")[0])]
         beacon = [int(split_line[1].split("=")[2]), int(split_line[1].split("=")[1].split(",")[0])]
 
-        sensor_beacon_pairs.append([sensor, beacon])
+        if sensor not in sensors:
+            sensors.append(sensor)
+        if beacon not in beacons:
+            beacons.append(beacon)
 
-        grid[sensor[0] + grid_radius][sensor[1] + grid_radius] = 'S'
-        grid[beacon[0] + grid_radius][beacon[1] + grid_radius] = 'B'
+        distance = manhattan_distance(sensor, beacon)
 
-    # for pair in sensor_beacon_pairs:
-    for pair in sensor_beacon_pairs:
-        mark_impossible_locations(grid, pair, grid_radius)
-        print('finished a bfs')
+        for row in range(sensor[0] - distance, sensor[0] + distance + 1):
+            if row == answer_row:
+                curr_interval = rows.get(row, interval())
+                interval_radius = distance - (abs(sensor[0] - row))
+                curr_interval = curr_interval | interval([sensor[1] - interval_radius, sensor[1] + interval_radius])
+                rows.update({row: curr_interval})
 
-    print(grid[2000000+grid_radius-1].count('#'))
+    interval_sums = 0
+    for intrvl in rows.get(answer_row):
+        interval_sums += intrvl.sup - intrvl.inf + 1
+        for sensor in sensors:
+            if sensor[0] == answer_row and sensor[1] in interval[intrvl.sup, intrvl.inf]:
+                interval_sums -= 1
+        for beacon in beacons:
+            if beacon[0] == answer_row and beacon[1] in interval[intrvl.sup, intrvl.inf]:
+                interval_sums -= 1
+
+    print(interval_sums)
 
 
 if __name__ == "__main__":
--- day_15/puzzle_2.py
+++ day_15/puzzle_2.py
@@ -1,9 +1,53 @@
+from interval import interval
+
+
+def manhattan_distance(vertex_one, vertex_two):
+    return abs(vertex_one[0] - vertex_two[0]) + abs(vertex_one[1] - vertex_two[1])
+
+
 def process_input():
     with open('inputs/puzzle_1.txt') as f:
         lines = f.readlines()
 
+    answer_row = 2000000
+
+    rows = {}
+
+    sensors = []
+    beacons = []
+
     for line in lines:
         line = line.replace('\n', '')
+        split_line = line.split(":")
+
+        sensor = [int(split_line[0].split("=")[2]), int(split_line[0].split("=")[1].split(",")[0])]
+        beacon = [int(split_line[1].split("=")[2]), int(split_line[1].split("=")[1].split(",")[0])]
+
+        if sensor not in sensors:
+            sensors.append(sensor)
+        if beacon not in beacons:
+            beacons.append(beacon)
+
+        distance = manhattan_distance(sensor, beacon)
+
+        for row in range(sensor[0] - distance, sensor[0] + distance + 1):
+            if 0 <= row <= 4000000:
+                curr_interval = rows.get(row, interval())
+                interval_radius = distance - (abs(sensor[0] - row))
+                curr_interval = curr_interval | interval([min(0, sensor[1] - interval_radius), max(4000000, sensor[1] + interval_radius)])
+                rows.update({row: curr_interval})
+
+    interval_sums = 0
+    for intrvl in rows.get(answer_row):
+        interval_sums += intrvl.sup - intrvl.inf + 1
+        for sensor in sensors:
+            if sensor[0] == answer_row and sensor[1] in interval[intrvl.sup, intrvl.inf]:
+                interval_sums -= 1
+        for beacon in beacons:
+            if beacon[0] == answer_row and beacon[1] in interval[intrvl.sup, intrvl.inf]:
+                interval_sums -= 1
+
+    print(interval_sums)
 
 
 if __name__ == "__main__":
