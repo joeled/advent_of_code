783c2edbfd73b7745e03dbc1933cd8dfdd57608f
Joe <joseph@orderlyhealth.com>
2022-12-16 15:07:39 -0700

    wip

--- day_15/puzzle_2.py
+++ day_15/puzzle_2.py
@@ -9,14 +9,17 @@ def process_input():
     with open('inputs/puzzle_1.txt') as f:
         lines = f.readlines()
 
-    answer_row = 2000000
-
     rows = {}
+    min_val = 0
+    max_val = 4000000
 
     sensors = []
     beacons = []
 
+    line_idx = 1
     for line in lines:
+        print("Processing line: " + str(line_idx))
+        line_idx += 1
         line = line.replace('\n', '')
         split_line = line.split(":")
 
@@ -31,24 +34,26 @@ def process_input():
         distance = manhattan_distance(sensor, beacon)
 
         for row in range(sensor[0] - distance, sensor[0] + distance + 1):
-            if 0 <= row <= 4000000:
+            if min_val <= row <= max_val:
                 curr_interval = rows.get(row, interval())
-                interval_radius = distance - (abs(sensor[0] - row))
-                curr_interval = curr_interval | interval([min(0, sensor[1] - interval_radius), max(4000000, sensor[1] + interval_radius)])
-                rows.update({row: curr_interval})
+                if len(curr_interval) == min_val or curr_interval[0].inf != 0 or curr_interval[0].sup != max_val:
+                    interval_radius = distance - (abs(sensor[0] - row))
+                    inf = max(min_val, sensor[1] - interval_radius)
+                    sup = min(max_val, sensor[1] + interval_radius)
+                    curr_interval = curr_interval | interval([inf, sup])
+                    rows.update({row: curr_interval})
 
-    interval_sums = 0
-    for intrvl in rows.get(answer_row):
-        interval_sums += intrvl.sup - intrvl.inf + 1
-        for sensor in sensors:
-            if sensor[0] == answer_row and sensor[1] in interval[intrvl.sup, intrvl.inf]:
-                interval_sums -= 1
-        for beacon in beacons:
-            if beacon[0] == answer_row and beacon[1] in interval[intrvl.sup, intrvl.inf]:
-                interval_sums -= 1
-
-    print(interval_sums)
+    for row in range(min_val, max_val+1):
+        test_interval = rows.get(row, interval())
+        contiguous = True
+        for idx in range(1, len(test_interval)):
+            if test_interval[idx - 1].sup != (test_interval[idx].inf - 1):
+                contiguous = False
+        if len(test_interval) > 1 and not contiguous:
+            print(row)
+            print(rows.get(row))
 
 
 if __name__ == "__main__":
     process_input()
+    print((4000000 * 3131431) + 2647448)
